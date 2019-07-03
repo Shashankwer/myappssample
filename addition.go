16f8d312fd3b5109713eb5c02a86ecbdd768971a
Shashankwer <shashanknigam40@gmail.com>
2019-07-04 00:18:17 +0800

    Initial commit

---
+++ addition.go
@@ -0,0 +1,22 @@
+package main 
+import "fmt"
+
+func add(x,y int) int {
+return x + y
+}
+
+func swap(x,y string)(string,string) {
+return y, x
+}
+
+
+func main() {
+fmt.Println(add(100,113))
+a,b := swap("Hello","World")
+fmt.Println(a,b)
+fmt.Println(split(17))
+k := 3
+fmt.Println(i,j,k,c,python,java)
+fmt.Printf("Type: %T Value: %v\n",ToBe,ToBe)
+fmt.Printf("Type: %T Value: %v\n",z,z)
+}
---
+++ var.go
@@ -0,0 +1,12 @@
+package main
+
+import "math/cmplx"
+var c,python,java = true,false, "no!"
+
+var i,j int = 1,2
+
+var (
+ ToBe bool = false
+ MaxInt uint64 = 1<<64 - 1
+ z complex128 = cmplx.Sqrt(-5 +12i)
+)
